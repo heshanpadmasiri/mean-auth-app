e5eab5c0e7b92c4319546af3111b0d77040a4bee
heshan <heshanpadmasiri@hotmail.com>
2018-01-30 20:04:41 +0530

    changed register route to a post route

--- routes/users.js
+++ routes/users.js
@@ -2,23 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 // Register
-router.get('/register',(req,res,next) => {
+router.post('/register',(req,res,next) => {
     res.send("Register request")
 });
 
 // Authenticate
-router.get('/authenticate',(req,res,next) => {
+router.post('/authenticate',(req,res,next) => {
     res.send("Authentication requseted")
 });
 
 // Profile
 router.get('/profile', (req,res,next) => {
     res.send("Profile requested")
-});
-
-// Validate
-router.get('/validate', (req,res,next) =>{
-    res.send("Validate")
 });
 
 module.exports = router;
