98708652de75c750356478d2fb0d1ccd1cd9ba6c
heshan <heshanpadmasiri@hotmail.com>
2018-02-04 14:43:54 +0530

    Added validation service to the project

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule }   from '@angular/forms';
 
+import { ValidateService } from './services/validate.service';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
@@ -34,7 +36,7 @@ const appRoutes: Routes = [
     FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ValidateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
