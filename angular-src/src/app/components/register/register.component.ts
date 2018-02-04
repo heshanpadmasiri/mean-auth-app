c846ad17b4e04f2cc3ada654f2921cb89ba3d6d6
heshan <heshanpadmasiri@hotmail.com>
2018-02-04 17:39:48 +0530

    registration functionality of the fron end working

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ValidateService} from '../../services/validate.service';
 import { AuthService } from '../../services/auth.service';
 import { FlashMessagesService } from 'angular2-flash-messages';
@@ -17,7 +18,8 @@ export class RegisterComponent implements OnInit {
   constructor(
     private validateService:ValidateService, 
     private flashMessagesServie:FlashMessagesService,
-    private auth:AuthService
+    private auth:AuthService,
+    private router:Router
   ) { }
 
   ngOnInit() {
@@ -38,14 +40,17 @@ export class RegisterComponent implements OnInit {
     }
 
     if(!this.validateService.validateEmail(user.email)){
-      this.flashMessagesServie.show('Invalid email address', {cssClass:'alert-danger', timeOut:3000})
+      this.flashMessagesServie.show('Invalid email address', {cssClass:'alert-danger', timeOut:3000});
       return false;
     }
 
-    this.auth.registerUser(user).subscribe(data =>{
-      console.log(data);
+    this.auth.registerUser(user).subscribe(data =>{      
       if(data.success){
-        console.log("Success")
+        this.flashMessagesServie.show("Successfully registered", {cssClass:'alert-success', timeOut:3000});
+        this.router.navigate(['/login']);
+      } else {
+        this.flashMessagesServie.show("Something went wrong", {cssClass:'alert-danger', timeOut:3000});
+        this.router.navigate(['/register']);
       }
     });
   }
